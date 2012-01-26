6980dcffd0bdb8c116842f12ed716df705d7661d
Ryan Davis <ryand-ruby@zenspider.com>
2012-01-26 14:38:45 -0800

    minor typos

--- simp-pkg.el
+++ simp-pkg.el
@@ -1,4 +1,4 @@
 (define-package
   "simp"
   "0.1.1"
-  "Simple project defenition, chiefly for project file finding and grepping.")
+  "Simple project definition, chiefly for project file finding and grepping.")
